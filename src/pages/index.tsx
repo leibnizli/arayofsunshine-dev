fa83b504eefae9344e8b86b30532e47976e55e41
Leibniz Li <stormte@gmail.com>
2022-08-31 11:38:06 +0800

    首页样式调整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ import Translate, {translate} from '@docusaurus/Translate';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero', styles.heroBanner)} >
+    <header className={clsx('hero hero--primary', styles.heroBanner)} >
       <div className="container">
         <h1 className={clsx('hero__title', styles.hero__title)}>GOTOMORROW</h1>
         <h2><Translate>Here you can find some gadgets</Translate></h2>
