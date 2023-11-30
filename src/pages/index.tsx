4ad3b503017e78e5cebf576ac1670e4ad8381fe7
Leibniz Li <stormte@gmail.com>
2023-11-30 23:32:34 +0800

    new home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,30 +9,51 @@ import Translate, {translate} from '@docusaurus/Translate';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)} >
-      <div className="container">
-        <h1 className={clsx('hero__title', styles.hero__title)}><Translate>A RAY OF SUNSHINE</Translate></h1>
-        <h2><Translate>Here you can find some gadgets</Translate></h2>
-        <p className="hero__subtitle">https://{siteConfig.tagline}</p>
+    <header className={clsx('hero hero--primary', styles.header)} >
+      <div className={clsx('container', styles.container)}>
+        <div>
+          <h1 className={clsx('hero__title', styles.hero__title)}><Translate>A RAY OF SUNSHINE</Translate></h1>
+          <h2><Translate>Here you can find some gadgets</Translate></h2>
+          <p className="hero__subtitle">https://{siteConfig.tagline}</p>
+        </div>
+        <div className={styles.headerImg}><img src="img/banner.svg" alt="banner"/></div>
       </div>
     </header>
+  );
+}
+function PCalculator() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <div className={clsx('hero', styles.PCalculator)} >
+      <div className={clsx('container', styles.container)}>
+        <div className={styles.PCalculatorImg}><img src="img/PCalculator.svg" alt="PCalculator"/></div>
+        <div  className={styles.PCalculatorBody}>
+          <h1 className={clsx('hero__title')}>PCalculator</h1>
+          <h2><Translate>A simple calculator</Translate></h2>
+          <p className="hero__subtitle">
+            <Link className="button button--secondary" to="/docs/RCalculator">
+              <Translate>Get Started</Translate>
+            </Link></p>
+        </div>
+      </div>
+    </div>
   );
 }
 function HomepageFooter() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero', styles.heroBanner)} >
+    <div className={clsx('hero', styles.heroBanner)} >
       <div className="container">
         <h1 className={clsx('hero__title')}>ConvertChineseCharacters</h1>
         <h2><Translate>Sublime text Chinese character and Unicode conversion plugin</Translate></h2>
         <p className="hero__subtitle"><Translate>It allowing you to convert your .js, .json, and .css files.</Translate></p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/ConvertChineseCharacters">
+          <Link className="button button--secondary" to="/docs/ConvertChineseCharacters">
             <Translate>Get Started</Translate>
           </Link>
         </div>
       </div>
-    </header>
+    </div>
   );
 }
 
@@ -49,7 +70,8 @@ export default function Home(): JSX.Element {
       <main>
         <HomepageFeatures />
       </main>
-      <HomepageFooter />
+      <PCalculator />
+      {/*<HomepageFooter />*/}
     </Layout>
   );
 }
